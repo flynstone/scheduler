df9a6ec2b8814f85058f6998cefd5f20f9281d2a
Julien <flynstone@live.ca>
2022-01-19 14:22:38 -0500

    commit interviewer list item component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import React from 'react';
+import "./InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) { 
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const interviewerClass = classNames(
+    "interviewers__item", { "interviewers__item--selected": selected }
+  );
+
+  return (
+    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+  
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      Sylvia Palmer
+    </li>
+  )
+};
