da7c2fed2d8d549c3c6d63083ada78e1ed3ba1c1
Julien <flynstone@live.ca>
2022-01-20 11:15:19 -0500

    commit diplaying appointments

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Button from '/src/components/Button';
+import InterviewerList from '/src/components/InterviewerList';
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer("");
+  }
+
+  const cancel = () => {
+    reset();
+    {props.onCancel()}
+  }
+
+  const handleEnterKey = (e) => {
+    if (e.key === 'Enter') {
+      {props.onSave()}
+    }
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+      <form onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(e) => setStudent(e.target.value)}
+            onKeyDown={handleEnterKey}
+            value={student}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}; 
