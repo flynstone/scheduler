403fc4b4abafeee1b64bd0412211c0e1b02e2eaa
Julien <flynstone@live.ca>
2022-01-20 11:24:55 -0500

    commit add schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DayList from './DayList';
+import Appointment from './Appointment/index.js';
 
 import "components/Application.scss";
 
@@ -21,6 +22,49 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 3,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+  },
+  {
+    id: 4,
+    time: '3pm',
+    interview: {
+      student: 'Archie Andrews',
+      interviewer: {
+        id: 4,
+        name: 'Cohana Roy',
+        avatar: 'https://i.imgur.com/FK8V841.jpg',
+      },
+    },
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+];
+
+const displayAppointments = appointments.map((x) => (
+  <Appointment key={x.id} {...x} />
+));
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
@@ -48,7 +92,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {displayAppointments}
       </section>
     </main>
   );
