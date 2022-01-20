75fc875edaa6e1066567c79f6b28112fd28a2507
Julien <flynstone@live.ca>
2022-01-20 09:58:26 -0500

    commit first appointment views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,8 @@
 import Appointment from "components/Appointment";
 import Header from "components/Header";
 import Empty from "./Empty";
+import Show from './Show';
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 import "./styles.scss";
